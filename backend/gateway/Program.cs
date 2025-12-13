da37506a414ab085faed10733dc2cd5aa4a8045e
Svetli03 <xwd21hqu@uea.ac.uk>
2025-12-13 13:08:43 +0000

    feature/0001 - Changing inappropriate comment :(

--- backend/gateway/Program.cs
+++ backend/gateway/Program.cs
@@ -16,11 +16,7 @@ builder.Services.AddControllers();
 
 var app = builder.Build();
 
-
-// DEEZ NUTS
-
-
-
+// Testing version control
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
