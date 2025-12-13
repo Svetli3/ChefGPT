6d255c00294c5b8b4035d32b0ca0e25b6317a580
yonneymuslia <Fatjonmuslia@hotmail.co.uk>
2025-12-13 13:25:06 +0000

    feature/0002 - Comment Fix

--- backend/gateway/Program.cs
+++ backend/gateway/Program.cs
@@ -16,11 +16,7 @@ builder.Services.AddControllers();
 
 var app = builder.Build();
 
-
-// DEEZ NUTS
 // Hello World
-
-
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
